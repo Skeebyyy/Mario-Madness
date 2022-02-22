f6d7d9e7d02eb7834193ed4775327b1e66724543
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2022-02-22 20:05:26 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -156,6 +156,9 @@ class CreditsState extends MusicBeatState
 		bg.color = creditsStuff[curSelected][4];
 		intendedColor = bg.color;
 		changeSelection();
+		
+		addVirtualPad(UP_DOWN, A_B);
+		
 		super.create();
 	}
 
